06d51586cc82ecec6c8c6d704aae2105a756a53b
Aaron Ostrovsky <aarondostrovsky+github@gmail.com>
2022-01-20 11:09:25 +0000

    init commit of a python script to read the test files I've written and run some simple trends and statistics with them

---
+++ sample_scripts/statistics.py
@@ -0,0 +1,28 @@
+#!/bin/python3
+
+import os
+import h5py as h5
+import pathlib as pl
+import sys
+import numpy as np
+import time
+
+file_order = []
+time_to_load = []
+
+inputDir = pl.Path("D:/B5D_tests/chunk_tests")
+files = os.listdir(inputDir)
+for f in files:
+	print(f)
+	file_order.append(f)
+	start = time.time()
+	loop_file = h5.File((inputDir / f),'r')
+	dset = loop_file['imagedata']
+	retreived_data_xy = dset[:,:,0,0,0]
+	end = time.time()
+	difference = end - start
+	print(difference)
+	time_to_load.append(difference)
+	loop_file.close()
+
+
